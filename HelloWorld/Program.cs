f6fe27c819ef62d8459b056467675f08b257535f
ks2019575042 <48661364+ks2019575042@users.noreply.github.com>
2021-03-08 09:44:32 +0900

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2019575042 윤진수");
+        }
+    }
+}
