2d1000a28717ed9566ac5b57824ee499c23a1fef
ks2019575042 <48661364+ks2019575042@users.noreply.github.com>
2021-03-22 10:42:37 +0900

    Add files via upload

---
+++ Basic/03Operator.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace Basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+            int first = 10, second = 20;
+            result = (first == second);
+            Console.WriteLine("{0} == {1} : {2}", first, second, result);
+            result = (first > second);
+            Console.WriteLine("{0} > {1} : {2}", first, second, result);
+            result = (first < second);
+            Console.WriteLine("{0} < {1} : {2}", first, second, result);
+            result = (first >= second);
+            Console.WriteLine("{0} >= {1} : {2}", first, second, result);
+            result = (first <= second);
+            Console.WriteLine("{0} <= {1} : {2}", first, second, result);
+            result = (first != second);
+            Console.WriteLine("{0} != {1} : {2}", first, second, result);
+        }
+    }
+}
